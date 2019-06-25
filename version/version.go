8b9428ea36cf57fb6f42591a19ed3f35e698c381
Anders Kvist <ak@cego.dk>
2019-06-25 22:16:23 +0200

    Updating info about ldflags.

--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
-// Version is overridden at compile time by `-ldflags "-X version.Version $VERSION"`
+// Version is overridden at compile time by `-ldflags "-X github.com/anderskvist/GoHelpers/version.Version=`date -u '+%Y%m%d-%H%M%S'`-`git rev-parse --short HEAD`"`
 var Version = "developer-build"
